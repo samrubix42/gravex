471086b3c67e502b902ea5446bc442b6cdcd97dc
Saumy Anand <samcool3203@gmail.com>
2026-03-06 14:54:12 +0530

    build file added

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::livewire('/','pages::home.home')->name('home');
 
+Route::get('clear-cache',function(){
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    Artisan::call('route:clear');
+    return redirect()->back();
+})
+
