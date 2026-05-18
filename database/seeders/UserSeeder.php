95d8588bd335abc622ce8071ef7955c341c38dd5
Saumy Anand <samcool3203@gmail.com>
2026-05-18 19:13:42 +0530

    user seeder updated

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-            ['email' => 'admin@grevx.com'],
+            ['email' => 'admin@grevx.co'],
             [
                 'name' => 'Admin User',
                 'password' => Hash::make('grevx765'),
